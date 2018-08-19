40d73f5904dea12d3770910adcbcf73945b84a94
Valon Haliti <valonfhaliti@gmail.com>
2018-08-19 02:48:09 +0200

    add body-parser

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-        message: 'Handling POST requests to /orders'
+        message: 'Handling POST requests to /orders',
+        order
     });
 });
 
--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Controll-Allow-Origin', '*');
+    res.header(
+        'Access-Controll-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Controll-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+})
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
