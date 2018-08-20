9ed8d85ca0726ecb19b64e5bed821cedfcc9e8a1
Valon Haliti <valonfhaliti@gmail.com>
2018-08-20 23:03:01 +0200

    Use connection pooling

--- dbconnection.js
+++ dbconnection.js
@@ -1,37 +1,36 @@
 const mysql = require('mysql');
+const util = require('util');
 
-class Database {
-    constructor() {
-        this.config = {
-            connectionLimit: 50,
-            host: process.env.DB_HOST_NAME,
-            port: process.env.DB_PORT,
-            user: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            insecureAuth: true
+const pool = mysql.createPool({
+    connectionLimit: 20,
+    host: process.env.DB_HOST_NAME,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        switch (err.code) {
+            case 'PROTOCOL_CONNECTION_LOST':
+                console.error('Database connection was closed.');
+                break;
+            case 'ER_CON_COUNT_ERROR':
+                console.error('Database has too many connections.');
+                break;
+            case 'ECONNREFUSED':
+                console.error('Database connection was refused .');
+                break;
         }
-        this.connection = mysql.createConnection(this.config);
     }
 
-    query(sql, args) {
-        return new Promise((resolve, reject) => {
-            this.connection.query(sql, args, (err, res) => {
-                if (err) return reject(err);
-                resolve(res);
-            })
-        });
-    }
+    if (connection) connection.release();
+    return;
+});
 
-    close() {
-        return new Promise((resolve, reject) => {
-            this.connection.end(err => {
-                if (err) reject(err);
-                resolve();
-            });
-        });
-    }
-}
+// Promisify for Node.js async/await.
+pool.query = util.promisify(pool.query)
 
-module.exports = Database;
-
+module.exports = pool
