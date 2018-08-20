fce63bdaac7e14481805c24571c8b8df71e97da2
Valon Haliti <valonfhaliti@gmail.com>
2018-08-20 23:03:23 +0200

    Use async handler for route handlers

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,53 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const asyncHandler = require('../utils/asyncHandler');
+const db = require('../../dbconnection');
 
-const Database = require('../../dbconnection');
-const db = new Database();
 
-router.get('/', async (req, res, next) => {    
-    try {
-        const rows = await db.query('SELECT * FROM orders');
-        res.status(200).json({
-            res: rows
-        });
-    } catch (err) {
-        res.status(500).json({ err });
-    }
-});
-
-router.post('/', (req, res, next) => {
+router.post('/', asyncHandler(async (req, res, next) => {
     const order = {
         productId: req.body.productId,
         quantity: req.body.quantity
     }
+    const rows = await db.query('INSERT INTO orders SET ?', order);
+    order.id = rows.insertId;
     res.status(201).json({
-        message: 'Handling POST requests to /orders',
-        order
+        createdOrder: order
     });
-});
+}));
 
-router.get('/:orderId', (req, res, next) => {
+
+router.get('/:orderId', asyncHandler(async (req, res, next) => {
     const id = req.params.orderId;
+    const rows = await db.query('SELECT * FROM orders where id = ?', id);
     res.status(200).json({
-        message: `Handling POST request to /orders/${id}!!`
+        res: rows
     });
-});
+}));
 
-router.patch('/:orderId', (req, res, next) => {
-    const id = req.params.orderId;
+router.get('/', asyncHandler(async (req, res, next) => {    
+    const rows = await db.query('SELECT * FROM orders');
     res.status(200).json({
-        message: `Handling POST request to /orders/${id}`
+        res: rows
     });
-});
+}));
 
 
-router.delete('/:orderId', (req, res, next) => {
-    const id = req.params.orderId;
-    res.status(200).json({
-        message: `Handling POST request to /orders/${id}`
-    });
-});
+router.patch('/:orderId', asyncHandler(async (req, res, next) => {
+    const updateOrder = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
+    await db.query('UPDATE orders SET ? WHERE id = ?', [updateOrder, req.params.orderId]);
+    updateOrder.id = req.param.orderId;
+    res.status(201).json({ updateOrder });
+}));
 
+
+router.delete('/:orderId', asyncHandler(async (req, res, next) => {
+    // FIXME: we should not delete from db, instead we should update (an attribute isdeleted for example)
+    await db.query('DELETE FROM orders WHERE id = ?', req.params.orderId);
+    res.status(201).json({ message: 'Successfully deleted!' });
+}));
 
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,73 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const asyncHandler = require('../utils/asyncHandler');
+const db = require('../../dbconnection');
 
-const Database = require('../../dbconnection');
-const db = new Database();
-// FIXME: this should not be here
 
-router.get('/', async (req, res, next) => {    
-    try {
-        const rows = await db.query('SELECT * FROM products');
-        res.status(200).json({
-            res: rows
-        });
-    } catch (err) {
-        res.status(500).json({ err });
-    }
-});
-
-router.post('/', async (req, res, next) => {
+router.post('/', asyncHandler(async (req, res, next) => {
     const product = {
         name: req.body.name,
         price: req.body.price
     }
-    try {
-        const rows = await db.query('INSERT INTO products SET ?', product);
-        product.id = rows.insertId;
-        res.status(201).json({
-            createdProduct: product
-        });
-    } catch (err) {
-        res.status(500).json({ err });
-    }
-});
+    const rows = await db.query('INSERT INTO products SET ?', product);
+    product.id = rows.insertId;
+    res.status(201).json({
+        createdProduct: product
+    });
+}));
 
-router.get('/:productId', async (req, res, next) => {
+
+router.get('/:productId', asyncHandler(async (req, res, next) => {
     const id = req.params.productId;
-    try {
-        const rows = await db.query('SELECT * FROM products where id = ?', id);
-        res.status(200).json({
-            res: rows
-        });
-    } catch (err) {
-        res.status(500).json({ err });
-    }
-});
+    const rows = await db.query('SELECT * FROM products where id = ?', id);
+    res.status(200).json({
+        res: rows
+    });
+}));
 
-router.patch('/:productId', async (req, res, next) => {
+router.get('/', asyncHandler(async (req, res, next) => {    
+    const rows = await db.query('SELECT * FROM products');
+    res.status(200).json({
+        res: rows
+    });
+}));
+
+
+router.patch('/:productId', asyncHandler(async (req, res, next) => {
     const updateProduct = {
         name: req.body.name,
         price: req.body.price
     }
-    try {
-        await db.query('UPDATE products SET ? WHERE id = ?', [updateProduct, req.params.productId]);
-        updateProduct.id = req.param.productId;
-        res.status(201).json({ updateProduct });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ err: 'error trying to update this product' });
-    }
-});
+    await db.query('UPDATE products SET ? WHERE id = ?', [updateProduct, req.params.productId]);
+    updateProduct.id = req.param.productId;
+    res.status(201).json({ updateProduct });
+}));
 
 
-router.delete('/:productId', async (req, res, next) => {
-    try {
-        await db.query('DELETE FROM products WHERE id = ?', req.params.productId);
-        res.status(201).json({ message: 'Successfully deleted!' });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ err: 'error trying to delete this product' });
-    }
-});
+router.delete('/:productId', asyncHandler(async (req, res, next) => {
+    // FIXME: we should not delete from db, instead we should update (an attribute isdeleted for example)
+    await db.query('DELETE FROM products WHERE id = ?', req.params.productId);
+    res.status(201).json({ message: 'Successfully deleted!' });
+}));
 
 module.exports = router;
