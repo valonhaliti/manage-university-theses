d50c696d1d8717e8e4a37f4b0ed3ec99b4ebd46a
Valon Haliti <valonfhaliti@gmail.com>
2018-08-25 16:44:39 +0200

    Fix delete API

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,46 +7,41 @@ const db = require('../../dbconnection');
 router.post('/', asyncHandler(async (req, res, next) => {
     const order = {
         productId: req.body.productId,
-        quantity: req.body.quantity
+        quantity: req.body.quantity,
+        is_deleted: 0
     }
-    const rows = await db.query('INSERT INTO orders SET ?', order);
+    const rows = await db.query('INSERT INTO orders SET ?;', order);
     order.id = rows.insertId;
-    res.status(201).json({
-        createdOrder: order
-    });
+    res.status(201).json({ createdOrder: order });
 }));
 
 
 router.get('/:orderId', asyncHandler(async (req, res, next) => {
     const id = req.params.orderId;
-    const rows = await db.query('SELECT * FROM orders where id = ?', id);
-    res.status(200).json({
-        res: rows
-    });
+    const rows = await db.query('SELECT * FROM orders WHERE is_deleted = 0 AND id = ?;', id);
+    res.status(200).json({ res: rows });
 }));
 
 router.get('/', asyncHandler(async (req, res, next) => {    
-    const rows = await db.query('SELECT * FROM orders');
-    res.status(200).json({
-        res: rows
-    });
+    const rows = await db.query('SELECT * FROM orders WHERE is_deleted = 0;');
+    res.status(200).json({ res: rows });
 }));
 
 
 router.patch('/:orderId', asyncHandler(async (req, res, next) => {
     const updateOrder = {
         productId: req.body.productId,
-        quantity: req.body.quantity
+        quantity: req.body.quantity,
+        is_deleted: 0
     }
-    await db.query('UPDATE orders SET ? WHERE id = ?', [updateOrder, req.params.orderId]);
+    await db.query('UPDATE orders SET ? WHERE id = ?;', [updateOrder, req.params.orderId]);
     updateOrder.id = req.param.orderId;
     res.status(201).json({ updateOrder });
 }));
 
 
 router.delete('/:orderId', asyncHandler(async (req, res, next) => {
-    // FIXME: we should not delete from db, instead we should update (an attribute isdeleted for example)
-    await db.query('DELETE FROM orders WHERE id = ?', req.params.orderId);
+    await db.query('UPDATE orders SET is_deleted = 1 WHERE id = ?;', req.params.orderId);
     res.status(201).json({ message: 'Successfully deleted!' });
 }));
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -7,46 +7,44 @@ const db = require('../../dbconnection');
 router.post('/', asyncHandler(async (req, res, next) => {
     const product = {
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        is_deleted: 0
     }
-    const rows = await db.query('INSERT INTO products SET ?', product);
+    const rows = await db.query('INSERT INTO products SET ?;', product);
     product.id = rows.insertId;
-    res.status(201).json({
-        createdProduct: product
-    });
+    res.status(201).json({ createdProduct: product });
 }));
 
 
 router.get('/:productId', asyncHandler(async (req, res, next) => {
     const id = req.params.productId;
-    const rows = await db.query('SELECT * FROM products where id = ?', id);
-    res.status(200).json({
-        res: rows
-    });
+    const rows = await db.query('SELECT * FROM products WHERE is_deleted = 0 AND id = ?;', id);
+    if (rows && rows.length > 0)
+        res.status(200).json({ res: rows });
+    else
+        res.status(404).json({ message: 'product not found' });
 }));
 
 router.get('/', asyncHandler(async (req, res, next) => {    
-    const rows = await db.query('SELECT * FROM products');
-    res.status(200).json({
-        res: rows
-    });
+    const rows = await db.query('SELECT * FROM products WHERE is_deleted = 0;');
+    res.status(200).json({ res: rows });
 }));
 
 
 router.patch('/:productId', asyncHandler(async (req, res, next) => {
     const updateProduct = {
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        is_deleted: 0
     }
-    await db.query('UPDATE products SET ? WHERE id = ?', [updateProduct, req.params.productId]);
+    await db.query('UPDATE products SET ? WHERE id = ?;', [updateProduct, req.params.productId]);
     updateProduct.id = req.param.productId;
     res.status(201).json({ updateProduct });
 }));
 
 
 router.delete('/:productId', asyncHandler(async (req, res, next) => {
-    // FIXME: we should not delete from db, instead we should update (an attribute isdeleted for example)
-    await db.query('DELETE FROM products WHERE id = ?', req.params.productId);
+    await db.query('UPDATE products SET is_deleted = 1 WHERE id = ?;', req.params.productId);
     res.status(201).json({ message: 'Successfully deleted!' });
 }));
 
