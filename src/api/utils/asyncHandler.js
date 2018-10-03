e0e6bd83ec4928477508fbc00eb84f8a099f006c
Valon Haliti <valonfhaliti@gmail.com>
2018-10-04 00:38:04 +0200

    Explain better asyncHandler function

--- src/api/utils/asyncHandler.js
+++ src/api/utils/asyncHandler.js
@@ -1,6 +1,6 @@
-// this is a wrapper function which for input takes a function 
-// for output gives a Promise, which resolves when that function has no error
-// and catches the error and passes that to next function if error has been throwned
+// this is a wrapper function which for input takes a function fn and
+// for output gives a Promise, which resolves the result of invokation of function fn
+// and catches the error if function fn throws and passes that to function next
 export default fn => (req, res, next) =>
   Promise
     .resolve(fn(req, res, next))
